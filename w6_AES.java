594bcae506298d24616d243c1d6e6a4b3d83a024
sruz18 <rnkumar0517@gmail.com>
2026-03-31 09:20:52 +0530

    updated

--- w6_AES.java
+++ w6_AES.java
@@ -1,55 +1,22 @@
-import java.util.Base64;
 import javax.crypto.*;
-import java.util.Scanner;
+import java.util.Base64;
 
 public class w6_AES {
+    public static void main(String[] args) throws Exception {
 
-    // Generate AES Key
-    public static SecretKey generateKey() throws Exception {
+        String text = "Hello, Rijndael AES!";
+
         KeyGenerator kg = KeyGenerator.getInstance("AES");
-        kg.init(128); // AES-128
-        return kg.generateKey();
-    }
+        SecretKey key = kg.generateKey();
 
-    // Encrypt
-    public static String encrypt(String text, SecretKey key) throws Exception {
         Cipher c = Cipher.getInstance("AES");
+
         c.init(Cipher.ENCRYPT_MODE, key);
-        byte[] enc = c.doFinal(text.getBytes());
-        return Base64.getEncoder().encodeToString(enc);
-    }
+        String enc = Base64.getEncoder().encodeToString(c.doFinal(text.getBytes()));
+        System.out.println("Encrypted: " + enc);
 
-    // Decrypt
-    public static String decrypt(String encryptedText, SecretKey key) throws Exception {
-        Cipher c = Cipher.getInstance("AES");
         c.init(Cipher.DECRYPT_MODE, key);
-        byte[] dec = c.doFinal(Base64.getDecoder().decode(encryptedText));
-        return new String(dec);
-    }
-
-    public static void main(String[] args) {
-
-        try {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("Enter text: ");
-            String text = sc.nextLine();
-
-            // Generate key (like first code)
-            SecretKey key = generateKey();
-
-            // Encrypt
-            String encrypted = encrypt(text, key);
-            System.out.println("\nEncrypted Text: " + encrypted);
-
-            // Decrypt
-            String decrypted = decrypt(encrypted, key);
-            System.out.println("Decrypted Text: " + decrypted);
-
-            sc.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String dec = new String(c.doFinal(Base64.getDecoder().decode(enc)));
+        System.out.println("Decrypted: " + dec);
     }
 }
