9651df75b1a1f7c60725e3a3b4303d082103b846
sruz <rnkumar0517@gmail.com>
2026-04-16 23:17:59 +0530

    Refactor Hill cipher encryption and decryption methods

--- complex/3/hillcipher.java
+++ complex/3/hillcipher.java
@@ -10,40 +10,55 @@ public class Q3_3_Hill {
 
         String r = "";
         for (int i = 0; i < t.length(); i += 2) {
-            int a = t.charAt(i) - 'A', b = t.charAt(i + 1) - 'A';
-            r += (char) (((k[0][0]*a + k[0][1]*b) % 26) + 'A');
-            r += (char) (((k[1][0]*a + k[1][1]*b) % 26) + 'A');
+            int a = t.charAt(i) - 'A';
+            int b = t.charAt(i + 1) - 'A';
+
+            r += (char)(((k[0][0]*a + k[0][1]*b) % 26) + 'A');
+            r += (char)(((k[1][0]*a + k[1][1]*b) % 26) + 'A');
         }
         return r;
     }
 
     static int inv(int a) {
-        for (int i = 1; i < 26; i++) if ((a * i) % 26 == 1) return i;
+        for (int i = 1; i < 26; i++)
+            if ((a * i) % 26 == 1)
+                return i;
         return 1;
     }
 
     static String decrypt(String t) {
-        int d = (k[0][0]*k[1][1] - k[0][1]*k[1][0]) % 26;
-        if (d < 0) d += 26;
+        int det = (k[0][0]*k[1][1] - k[0][1]*k[1][0]) % 26;
+        if (det < 0) det += 26;
 
-        int id = inv(d);
+        int invDet = inv(det);
 
-        int[][] i = {
-            {(k[1][1]*id)%26, (26 - k[0][1]*id%26)%26},
-            {(26 - k[1][0]*id%26)%26, (k[0][0]*id)%26}
-        };
+        int[][] inv = new int[2][2];
+
+        inv[0][0] = (k[1][1] * invDet) % 26;
+        inv[0][1] = (-k[0][1] * invDet) % 26;
+        inv[1][0] = (-k[1][0] * invDet) % 26;
+        inv[1][1] = (k[0][0] * invDet) % 26;
+
+        // Fix negative values
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                if (inv[i][j] < 0)
+                    inv[i][j] += 26;
 
         String r = "";
-        for (int j = 0; j < t.length(); j += 2) {
-            int a = t.charAt(j) - 'A', b = t.charAt(j + 1) - 'A';
-            r += (char) (((i[0][0]*a + i[0][1]*b) % 26) + 'A');
-            r += (char) (((i[1][0]*a + i[1][1]*b) % 26) + 'A');
+        for (int i = 0; i < t.length(); i += 2) {
+            int a = t.charAt(i) - 'A';
+            int b = t.charAt(i + 1) - 'A';
+
+            r += (char)(((inv[0][0]*a + inv[0][1]*b) % 26) + 'A');
+            r += (char)(((inv[1][0]*a + inv[1][1]*b) % 26) + 'A');
         }
         return r;
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter text: ");
         String t = s.nextLine();
 
         String e = encrypt(t);
